b85b37fa26ac0e187a32f1466b3a2cad375ed56e
Pip Karoly <pkaroly@student.unimelb.edu.au>
2018-05-31 11:36:07 +1000

    typo

--- src/ios/MinewTrackerkit.m
+++ src/ios/MinewTrackerkit.m
@@ -10,9 +10,6 @@
 
 - (void)pluginInitialize {
 
-    NSLog(@"Cordova BLE Central Plugin");
-    NSLog(@"(c)2014-2016 Don Coleman");
-
     [super pluginInitialize];
 
     peripherals = [NSMutableSet set];
